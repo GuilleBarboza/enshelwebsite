f866828ea0d08bad33517252a8f7af0302dd127a
Guishe <gui.bar.jim@gmail.com>
2020-05-22 11:22:40 -0300

    fixing banner src path

--- js/littledetails.js
+++ js/littledetails.js
@@ -7,7 +7,7 @@ const root = document.getElementById('root');
 const header = document.createElement('header');
 
 const banner1 = document.createElement('img');
-banner1.src = data.banners.bannerPrincipal;
+banner1.src = data.banners.imagenes.bannerPrincipal;
 
 root.appendChild(header);
 header.appendChild(banner1);
